1edf3f6ae4144c185705bd360b8300cd70b08425
nimmimathon <nirmal93.kaur93@gmail.com>
2021-01-13 09:32:32 +0100

    13 Jan

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 30Nov
+this is new Read me file from 13th January
---
+++ StorageFunctionLibrary.psm1
@@ -0,0 +1,9 @@
+﻿. (Join-Path $PSScriptRoot "private/Get-AzureStorage.ps1")
+. (Join-Path $PSScriptRoot "public/Get-AppINFOMain.ps1")
+. (Join-Path $PSScriptRoot "public/Download-DependentApps.ps1")
+. (Join-Path $PSScriptRoot "public/Get-DependentApp.ps1")
+. (Join-Path $PSScriptRoot "public/Get-InstalledApp.ps1")
+. (Join-Path $PSScriptRoot "public/Compile-NAVApp.ps1")
+. (Join-Path $PSScriptRoot "public/Get-NAVSymbols.ps1")
+. (Join-Path $PSScriptRoot "public/Install-NAVApp.ps1")
+
