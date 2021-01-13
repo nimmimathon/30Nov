aa15b70d9b26a0b08876661fe18a6c31ba914b36
nimmimathon <nirmal93.kaur93@gmail.com>
2021-01-13 09:40:40 +0100

    test

--- StorageFunctionLibrary.psm1
+++ StorageFunctionLibrary.psm1
@@ -2,8 +2,9 @@
 . (Join-Path $PSScriptRoot "public/Get-AppINFOMain.ps1")
 . (Join-Path $PSScriptRoot "public/Download-DependentApps.ps1")
 . (Join-Path $PSScriptRoot "public/Get-DependentApp.ps1")
-. (Join-Path $PSScriptRoot "public/Get-InstalledApp.ps1")
 . (Join-Path $PSScriptRoot "public/Compile-NAVApp.ps1")
 . (Join-Path $PSScriptRoot "public/Get-NAVSymbols.ps1")
 . (Join-Path $PSScriptRoot "public/Install-NAVApp.ps1")
 
+. (Join-Path $PSScriptRoot "public/Install-NAVApp.ps2")
+. (Join-Path $PSScriptRoot "public/Install-NAVApp.ps3")
